03a262cf33e3c6b96f91e5d394b369cfced2d8d9
Krysh <krysh.developer@gmail.com>
2022-09-20 17:10:17 +0200

    [DEV-778] removed API client

--- core/cold-wallet/wallet/cold-wallet.ts
+++ core/cold-wallet/wallet/cold-wallet.ts
@@ -2,7 +2,14 @@ import { Network } from '@defichain/jellyfish-network';
 import { JellyfishWallet, WalletHdNode } from '@defichain/jellyfish-wallet';
 import { WhaleWalletAccount, WhaleWalletAccountProvider } from '@defichain/whale-api-wallet';
 import { Bip32Options, MnemonicHdNodeProvider } from '@defichain/jellyfish-wallet-mnemonic';
-import { WhaleApiClient } from '@defichain/whale-api-client';
+import {
+  ApiPagedResponse,
+  Method,
+  ResponseAsString,
+  WhaleApiClient,
+  WhaleApiResponse,
+} from '@defichain/whale-api-client';
+import { Logger } from '../../shared/logger';
 
 export class ColdWallet {
   public static NEEDED_SEED_LENGTH = 24;
@@ -17,7 +24,7 @@ export class ColdWallet {
     this.network = network;
     // TODO (Krysh) remove this client as soon as serial communication is setup
     // cold wallet should not need to have internet access
-    this.client = new WhaleApiClient({
+    this.client = new ColdWalletClient({
       url: process.env.OCEAN_URL,
       version: process.env.OCEAN_VERSION,
       network: network.name,
@@ -28,7 +35,7 @@ export class ColdWallet {
     if (!this.checkPrerequisites()) throw new Error('Seed is invalid');
     this.wallet = new JellyfishWallet(
       MnemonicHdNodeProvider.fromWords(this.seed, this.bip32OptionsBasedOn(this.network)),
-      new WhaleWalletAccountProvider(this.client, this.network),
+      new WhaleWalletAccountProvider(undefined as unknown as WhaleApiClient, this.network), // if crashes occur change to ColdWalletClient, need to overwrite all properties
       JellyfishWallet.COIN_TYPE_DFI,
       JellyfishWallet.PURPOSE_LIGHT_MASTERNODE,
     );
@@ -51,5 +58,34 @@ export class ColdWallet {
 
   private checkPrerequisites(): boolean {
     return this.seed != null && this.seed.length === ColdWallet.NEEDED_SEED_LENGTH;
+  }
+}
+
+class ColdWalletClient extends WhaleApiClient {
+  private readonly logger = new Logger('Cold Wallet Client');
+
+  paginate<T>(response: ApiPagedResponse<T>): Promise<ApiPagedResponse<T>> {
+    this.logger.info('paginate');
+    return super.paginate(response);
+  }
+
+  requestData<T>(method: Method, path: string, object?: any): Promise<T> {
+    this.logger.info('requestData');
+    return super.requestData(method, path, object);
+  }
+
+  requestList<T>(method: Method, path: string, size: number, next?: string | undefined): Promise<ApiPagedResponse<T>> {
+    this.logger.info('requestList');
+    return super.requestList(method, path, size, next);
+  }
+
+  requestAsApiResponse<T>(method: Method, path: string, object?: any): Promise<WhaleApiResponse<T>> {
+    this.logger.info('requestAsApiResponse');
+    return super.requestAsApiResponse(method, path, object);
+  }
+
+  requestAsString(method: Method, path: string, body?: string | undefined): Promise<ResponseAsString> {
+    this.logger.info('requestAsString');
+    return super.requestAsString(method, path, body);
   }
 }
