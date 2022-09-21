4f9f131134abda6275b1b1aaabe47f5dcfc0e5c0
Krysh <krysh.developer@gmail.com>
2022-09-21 12:43:41 +0200

    [DEV-779] added broadcasting of txHex

--- core/masternode-manager/main.ts
+++ core/masternode-manager/main.ts
@@ -1,22 +1,30 @@
 import { exit } from 'process';
-import { Operation } from '../shared/communication/operation';
+import { Operation, TestPayload } from '../shared/communication/operation';
 import { Logger } from '../shared/logger';
 import { Util } from '../shared/util';
 import { ColdWalletCommunication } from './cold-wallet-communication';
+import { WhaleApiClient } from '@defichain/whale-api-client';
+import Config from '../shared/config';
 
 class App {
   private readonly communication: ColdWalletCommunication;
   private readonly logger: Logger;
+  private readonly client: WhaleApiClient;
 
   constructor() {
     this.communication = new ColdWalletCommunication();
     this.logger = new Logger('Masternode Manager');
+    this.client = new WhaleApiClient({
+      url: Config.ocean.url,
+      version: Config.ocean.version,
+      network: Util.readNetwork().name,
+    });
   }
 
   async run(): Promise<void> {
     await this.communication.connect();
-    const message = await this.communication.query(Operation.TEST, undefined);
-    this.logger.info('received answer', message);
+    const message = (await this.communication.query(Operation.TEST, undefined)) as TestPayload;
+    await this.client.rawtx.send({ hex: message.txHex });
 
     try {
       while (true) {
