652d174ee209dda018af7b23af709259c5eaedd0
David May <david.leo.may@gmail.com>
2022-10-11 11:03:32 +0200

    [DEV-792] Added UI to main

--- cold-wallet/main.ts
+++ cold-wallet/main.ts
@@ -2,6 +2,8 @@ import { exit } from 'process';
 import { Operation } from '../shared/communication/operation';
 import { RawTxDto } from '../shared/dto/raw-tx.dto';
 import { Logger } from '../shared/logger';
+import { KeyInput } from '../shared/peripheral/key-input';
+import { Color, Led } from '../shared/peripheral/led';
 import { Util } from '../shared/util';
 import { ManagerCommunication } from './communication/manager-communication';
 import { WalletHelper } from './wallet/wallet-helper';
@@ -10,26 +12,48 @@ class App {
   private readonly communication: ManagerCommunication;
   private readonly logger: Logger;
 
+  private readonly led: Led;
+
   constructor() {
     this.communication = new ManagerCommunication();
     this.logger = new Logger('Cold Wallet');
+    this.led = new Led();
   }
 
   async run(): Promise<void> {
-    const wallet = await WalletHelper.restore();
-    wallet.initialize();
+    try {
+      // setup UI
+      await this.led.connect();
 
-    this.communication.on(Operation.RECEIVE_WALLET_NAME, () => wallet.getName());
-    this.communication.on(Operation.SIGN_TX, (data: RawTxDto) => wallet.signTx(data));
+      const input = new KeyInput(this.led);
+      await input.connect();
 
-    await this.communication.connect();
+      // get the seed pass code
+      await this.led.blink(Color.BLUE);
+      const code = await input.readLine();
+      if (code.match(/[^0-9]/)) throw new Error('Only numbers are allowed');
 
-    for (;;) {
-      try {
+      // setup wallet and communication
+      await this.led.blink(Color.YELLOW);
+
+      const wallet = await WalletHelper.restore(code);
+      wallet.initialize();
+
+      this.communication.on(Operation.RECEIVE_WALLET_NAME, () => wallet.getName());
+      this.communication.on(Operation.SIGN_TX, (data: RawTxDto) => wallet.signTx(data));
+
+      await this.communication.connect();
+
+      // wallet up
+      await this.led.set(Color.GREEN);
+
+      // infinite loop
+      for (;;) {
         await Util.sleep(5);
-      } catch (e) {
-        this.logger.error(`Exception: ${e}`);
       }
+    } catch (e) {
+      this.logger.error(`Exception: ${e}`);
+      await this.led.set(Color.RED);
     }
   }
 }
