bc852e21eb8a261d060c0c729c46cdbb952ef9ca
David May <david.leo.may@gmail.com>
2022-10-12 11:43:51 +0200

    [DEV-863] Added util to decrypt shamir share

---
+++ util/decrypt-shamir.ts
@@ -0,0 +1,24 @@
+import { exit } from 'process';
+import Config from '../shared/config';
+import { Encryption } from '../shared/crypto/encryption';
+import { Util } from '../shared/util';
+
+class App {
+  async run(): Promise<void> {
+    const code = await Util.getCliInput('Please enter the pass code:', true);
+    const share = Util.readFileRaw(Config.wallet.seed.readFilePath(0));
+    const decryptedShare = code ? Encryption.decrypt(share, code) : share;
+
+    if (decryptedShare.match(/^\d/)) {
+      console.log('OK');
+      console.log('Shamir share:', decryptedShare);
+    } else {
+      console.log('NOK');
+    }
+  }
+}
+
+new App()
+  .run()
+  .catch(console.error)
+  .finally(() => exit());
