86a98bc5e418c5b05ec4e377725f63c3936827d3
Krysh <krysh.developer@gmail.com>
2022-10-13 12:02:27 +0200

    [DEV-866] activating real communication

--- cold-wallet/communication/gateway-communication.ts
+++ cold-wallet/communication/gateway-communication.ts
@@ -1,15 +1,15 @@
-// import { BaseSerialCommunication } from '../../shared/communication/base-serial-communication';
+import { BaseSerialCommunication } from '../../shared/communication/base-serial-communication';
 
-import { BaseServerCommunication } from '../../shared/communication/base-server-communication';
+// import { BaseServerCommunication } from '../../shared/communication/base-server-communication';
 
-// export class GatewayCommunication extends BaseSerialCommunication {
-//   getPath(): string {
-//     return '/dev/serial0';
-//   }
-// }
-
-export class GatewayCommunication extends BaseServerCommunication {
-  listenOnPort(): number {
-    return 9000;
+export class GatewayCommunication extends BaseSerialCommunication {
+  getPath(): string {
+    return '/dev/serial0';
   }
 }
+
+// export class GatewayCommunication extends BaseServerCommunication {
+//   listenOnPort(): number {
+//     return 9000;
+//   }
+// }
--- gateway/cold-wallet-communication.ts
+++ gateway/cold-wallet-communication.ts
@@ -1,14 +1,14 @@
-// import { BaseSerialCommunication } from '../shared/communication/base-serial-communication';
-import { BaseSocketCommunication } from '../shared/communication/base-socket-communication';
+import { BaseSerialCommunication } from '../shared/communication/base-serial-communication';
+// import { BaseSocketCommunication } from '../shared/communication/base-socket-communication';
 
-// export class ColdWalletCommunication extends BaseSerialCommunication {
-//   getPath(): string {
-//     return '/dev/cu.usbserial-01434108';
-//   }
-// }
-
-export class ColdWalletCommunication extends BaseSocketCommunication {
-  connectToPort(): number {
-    return 9000;
+export class ColdWalletCommunication extends BaseSerialCommunication {
+  getPath(): string {
+    return '/dev/cu.usbserial-01434108';
   }
 }
+
+// export class ColdWalletCommunication extends BaseSocketCommunication {
+//   connectToPort(): number {
+//     return 9000;
+//   }
+// }
